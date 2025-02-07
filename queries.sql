14e673fed273391808ea9f22c5af330c6aa45d8c
rachelal1en <166883143+rachelal1en@users.noreply.github.com>
2025-02-06 19:03:03 -0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -3,7 +3,9 @@ select COLUMN_NAME, DATA_TYPE
 from INFORMATION_SCHEMA.COLUMNS
 where TABLE_NAME = 'job';
 --Part 2
-
+SELECT name 
+FROM employer 
+WHERE location = "St. Louis City";
 --Part 3
 
 --Part 4
